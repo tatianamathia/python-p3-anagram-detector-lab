cca5d6c4fe7c77c36ab72a6a0aef2d02bab069f2
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-27 00:09:37 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
-# your code goes here!
+# create anagram class
+
+
+class Anagram:
+    def __init__(self, word):
+      
+        self.word = word.lower()
+
+    def match(self, word_list):
+        
+        sorted_word = sorted(self.word)
+
+       
+        anagrams = []
+
+       
+        for candidate in word_list:
+            
+            if sorted(candidate.lower()) == sorted_word:
+            
+                anagrams.append(candidate)
+
+        return anagrams
